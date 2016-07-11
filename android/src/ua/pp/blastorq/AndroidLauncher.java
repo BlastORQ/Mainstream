4bb89e7e66f5fa72e902bdf8767d71dd4ec140c4
senjka4 <senjka4@gmail.com>
2016-07-11 17:09:46 +0300

    Налаштував проект

--- android/src/ua/pp/blastorq/AndroidLauncher.java
+++ android/src/ua/pp/blastorq/AndroidLauncher.java
@@ -11,6 +11,11 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useGyroscope = false;
+		config.useGLSurfaceView20API18 = false;
+		config.useImmersiveMode = false;
 		initialize(new Mainstream(), config);
 	}
 }
--- desktop/src/ua/pp/blastorq/desktop/DesktopLauncher.java
+++ desktop/src/ua/pp/blastorq/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ua.pp.blastorq.Mainstream;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Mainstream";
+		config.width = 320;
+		config.height = 630;
 		new LwjglApplication(new Mainstream(), config);
 	}
 }

d01dc27b0ee3b13a43a5fd6718abf4f65ed60026
Manzhula David <da411d@gmail.com>
2016-07-29 22:43:06 +0300

    Видалив мусорний код, додав змінну для виключання зіткнень

--- core/src/ua/pp/blastorq/mainstream/game/GameWorld.java
+++ core/src/ua/pp/blastorq/mainstream/game/GameWorld.java
@@ -11,32 +11,24 @@ import ua.pp.blastorq.mainstream.screens.GameScreen;
  * Created by serhij on 25.07.2016.
  */
 public class GameWorld {
+    public final boolean DEMO = true;//Це міняй якщо не треба реагувати на зіткнення
     int midPointX;
     int midPointY;
     Fly fly;
     MovHandler movHandler;
-  //  private Rectangle ground;
     public GameWorld(int midPointX, int midPointY) {
         this.midPointX = midPointX;
         this.midPointY = midPointY;
         fly = new Fly(40 ,midPointY + 5 ,17 ,17);
         movHandler = new MovHandler(this ,midPointY + 66);
-//        ground  = new Rectangle(0 ,0 , new GameScreen().getGameWidth() ,11);
     }
 
     public void update(float delta){
-        if(movHandler.collides(fly) && fly.isAlive()){
-           // fly.die();
-           // fly.clie();
-           //movHandler.stop();
-        }
-       /* if(fly.getRectangle().overlaps(ground)){
-            if(fly.isAlive){
-                fly.die();
-            }
-            movHandler.stop();
+        if(movHandler.collides(fly) && fly.isAlive() && !DEMO){
+            fly.die();
             fly.clie();
-        }*/
+           movHandler.stop();
+        }
         fly.update(delta);
         movHandler.update(delta);
     }
